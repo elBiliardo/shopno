6b8a15031f208730753e6b3b4a5f09e511774334
elBiliardo <hnatkivpublic@gmail.com>
2021-09-25 16:48:09 +0300

    modified aboutUs

--- src/Components/02AboutUs/AboutUsSlider/AboutUsSlider.jsx
+++ src/Components/02AboutUs/AboutUsSlider/AboutUsSlider.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
@@ -7,11 +7,6 @@ import styles from './AboutUsSlider.module.scss'
 import './AboutUsSliderDots.scss'
 
 export const AboutUsSlider = ({ slides }) => {
-	const slider = useRef(null)
-	const socus = () => {
-		console.log(slider)
-		slider.current.focus()
-	}
 
 	const settings = {
 		arrows: false,
@@ -24,11 +19,10 @@ export const AboutUsSlider = ({ slides }) => {
 		autoplay: true,
 		customPaging: () => <div className='dotItemAboutUs'></div>,
 		dotsClass: 'slick-dots dotsListAboutUs',
-		onSwipe: () => {socus()}
 	}
 
 	return (
-		<div className={styles.aboutUsSliderContainer} ref={slider} tabIndex={1}>
+		<div className={styles.aboutUsSliderContainer}>
 			<Slider {...settings} className={styles.aboutUsSlider}>
 				{slides.map(slide => <div className={styles.slideItem} key={slide.id}>
 					<img src={slide.img} alt={`slide${slide.id}`} />
